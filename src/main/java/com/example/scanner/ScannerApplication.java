ac37f95769d5d771fd1bb1fe8d7ccd208f44de55
zmad5306 <zachary.maddox@gmail.com>
2018-08-24 17:07:55 -0400

    finally doing some web socket comms

--- src/main/java/com/example/scanner/ScannerApplication.java
+++ src/main/java/com/example/scanner/ScannerApplication.java
@@ -1,9 +1,21 @@
 package com.example.scanner;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.InputStream;
 import java.net.URI;
+import java.nio.ByteBuffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -15,6 +27,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.socket.BinaryMessage;
 import org.springframework.web.socket.WebSocketHttpHeaders;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.client.WebSocketClient;
@@ -27,6 +40,17 @@ public class ScannerApplication implements CommandLineRunner {
 	private final String authorizationHeaderFormat = "Bearer %s";
 	private final String endpointURI = "wss://speech.platform.bing.com/speech/recognition/conversation/cognitiveservices/v1?language=en-US";
 	private final Logger logger = LoggerFactory.getLogger(ScannerApplication.class);
+	private final String chunkHeader = 
+			"Path:audio\r\n" + 
+			"X-Timestamp:%s\r\n" + 
+			"Content-Type:audio/x-wav\r\n" + 
+			"X-RequestId:%s\r\n";
+	
+	
+//	path:audio
+//	x-timestamp:2018-08-24T18:23:25.664Z
+//	content-type:audio/x-wav
+//	x-requestid:0a26bf38e33e4f508fb8f9fbcff5a424
 	
 	private String token;
 	private String connectionId;
@@ -58,12 +82,41 @@ public class ScannerApplication implements CommandLineRunner {
     	return connectionId;
     }
     
+    private String getMessageId() {
+    	return UUID.randomUUID().toString().replace("-", "");
+    }
+    
     private WebSocketHttpHeaders getHeaders() {
     	WebSocketHttpHeaders headers = new WebSocketHttpHeaders();
     	headers.put("Authorization", Arrays.asList(getAuthorizationHeader()));
 		headers.put("X-ConnectionId", Arrays.asList(getConnectionId()));
     	return headers;
     }
+    
+    private String buildNowHeader() {
+    	TimeZone tz = TimeZone.getTimeZone("UTC");
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'"); // Quoted "Z" to indicate UTC, no timezone offset
+		df.setTimeZone(tz);
+		String nowAsISO = df.format(new Date());
+		return nowAsISO;
+    }
+    
+	private BinaryMessage createMessage(byte[] buffer) {
+	  /* Headers:
+	      [{'path':'audio'},
+	      {'x-timestamp':Date.UTC(new Date().toISOString())},
+	      {'content-type': 'audio/x-wav'},
+	      {'x-requestid': connectionId}];
+	  */
+		String headers = String.format(chunkHeader, buildNowHeader(), getMessageId());
+		Short hdrLength = Integer.valueOf(headers.length()).shortValue();
+		
+		byte[] headerLength = ByteBuffer.allocate(2).putShort(hdrLength).array();
+
+		byte[] data = ArrayUtils.addAll(headerLength, headers.getBytes(Charset.forName("utf8")));
+		data = ArrayUtils.addAll(data, buffer);
+		return new BinaryMessage(data);
+	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(ScannerApplication.class, args);
@@ -72,6 +125,17 @@ public class ScannerApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		WebSocketClient client = new StandardWebSocketClient();
-		WebSocketSession session = client.doHandshake(new SpeechWebSocketHandler(), getHeaders(), new URI(endpointURI)).get();
+		WebSocketSession session = client.doHandshake(new SpeechWebSocketHandler(), getHeaders(), new URI(endpointURI)).get(30, TimeUnit.SECONDS);
+		
+		InputStream in = Files.newInputStream(new File("C:\\Users\\Zach\\Desktop\\Repos\\scanner-convert\\samples\\broadcastify01.wav").toPath());
+		byte[] buffer = new byte[8192];
+		int len = 0;
+		while((len = in.read(buffer)) != -1) {
+			logger.trace("read {} bytes", len);
+			BinaryMessage message = createMessage(buffer);
+			session.sendMessage(message);
+		}
 	}
+
+
 }
--- src/main/java/com/example/scanner/SpeechWebSocketHandler.java
+++ src/main/java/com/example/scanner/SpeechWebSocketHandler.java
@@ -1,26 +1,34 @@
 package com.example.scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 
 public class SpeechWebSocketHandler implements WebSocketHandler {
+	
+	private final Logger logger = LoggerFactory.getLogger(SpeechWebSocketHandler.class);
 
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		logger.info("connection established, session id: {}", session.getId());
 	}
 
 	@Override
 	public void handleMessage(WebSocketSession session, WebSocketMessage<?> message) throws Exception {
+		logger.info("message recieved, session id: {}, length: {}, payload: {}", session.getId(), message.getPayloadLength(), message.getPayload());
 	}
 
 	@Override
 	public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+		logger.error("transport error {} on session {}", exception.getMessage(), session.getId());
 	}
 
 	@Override
 	public void afterConnectionClosed(WebSocketSession session, CloseStatus closeStatus) throws Exception {
+		logger.info("connection closed on session {} with status {} {}", session.getId(), closeStatus.getCode(), closeStatus.getReason());
 	}
 
 	@Override
